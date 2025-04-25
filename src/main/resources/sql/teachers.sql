25b0ae4b80a74fde487d572f0d27942da326131d
BaltasisKos <Baltasiskostas@hotmail.com>
2025-04-25 21:03:42 +0300

    Initial commit

---
+++ src/main/resources/sql/teachers.sql
@@ -0,0 +1,10 @@
+USE hiber7prodb;
+
+INSERT INTO `teachers` (id, firstname, lastname) VALUES
+    (1,'Αθανάσιος','Ανδρούτσος'),
+	(2,'Μάκης','Καπέτης'),
+	(3,'Μάρκος','Καραμπάτσης'),
+	(4,'Παναγιώτης','Μόσχος'),
+	(5,'Σοφοκλής','Στουραϊτης');
+
+ALTER TABLE teachers AUTO_INCREMENT = 11;
